77c20898159c670fe31ed83ae5cd53a3af0e48ca
kevin <outaskevin@gmail.com>
2023-01-01 20:10:18 -0800

    update

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * _abs - com[putes the absolute value of an integer
+  * @c: The number to be computed
+  * Return: Absolute value of a number or 0
+  */
+
+int _abs(int c)
+{
+	if (c < 0)
+	{
+		int abs_val;
+
+		abs_val = c * -1;
+		return (abs_val);
+	}
+	return (c);
+}
