87c4e40e5c0b154dd4a927a977991bca7517944c
kevin <outaskevin@gmail.com>
2023-01-03 16:43:32 -0800

    even more pointers

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * _memcpy - entry point
+  * @dest: input
+  * @src: input
+  * @n: input
+  * Return: O always (success)
+  */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
+}
